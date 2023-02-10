d22692c781bff5389bdf1ca047e8fefce051875a
Abel Apriliani <123937217+abelaprl@users.noreply.github.com>
2023-02-11 00:08:02 +0700

    Menjawab Tugas Review

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,24 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
-
+    printf(" 1. Int (integer) : tipe data yang nilainya terdiri dari bilangan bulat, 2. Float (real number) : tipe data angka yang memiliki bagian desimal di akhir angka \n"); // 
+    printf (" 3. Double : menyimpan angka yang mengandung pecahan. Tipe float hanya bisa menampung 7 angka dibelakang koma, sedangkan double bisa menampung hingga 15 angka dibelakang koma. \n");
+    printf ("4. Boolean : tipe data yang hanya memiliki 2 pilihan nilai, yaitu true dan false. \n");
+    printf ("5. Char : tipe data yang digunakan untuk menyimpan satu karakter saja. Dan penulisannya harus diapit oleh tanda petik tunggal. \n");
+    printf ("6. String : digunakan untuk menyimpan data karakter yang berupa teks (sama seperti char). \n");
+    printf ("7. Void : digunakan untuk fungsi-fungsi yang tidak mengembalikan nilai. \n");
+    printf ("8. Wide_character, Tipe data karakter lebar juga merupakan tipe data karakter tetapi tipe data ini memiliki ukuran lebih besar dari tipe data 8-bit normal. Panjang umumnya 2 atau 4 byte. \n");
+    
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
-
+    printf(" Pada prosedur nilai yang diproses tidak dikembalikan atau diubah, nilai hanya diproses didalam prosedur dan kembali kepada keadaan semula ketika keluar dari prosedur. Sedangkan pada fungsi, nilai yang diproses akan berubah dan memiliki nilai keluaran.\n"); 
+    
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Mendefinisikan/ deklarasi pointer ke pointer ke integer \n");
 
     return 0;
 }
